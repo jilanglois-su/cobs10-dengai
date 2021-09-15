3299b3ba4b2a1de29e93193d4bce0daa9d06d726
jilanglois <56458805+jilanglois@users.noreply.github.com>
2021-09-14 18:28:35 -0700

    update test poisson glm

--- tests/test_poisson_glm.py
+++ tests/test_poisson_glm.py
@@ -1,14 +1,26 @@
 from unittest import TestCase
-import os
-import seaborn as sns
-import pandas as pd
+import numpy as np
 from src.d01_data.dengue_data_api import DengueDataApi
 from src.d04_modeling.poisson_glm import PoissonGLM
 
 
-class TestModels(TestCase):
+class TestPoissonGlm(TestCase):
 
-    def test_poisson_glm(self):
+    def test_loglikeihood(self):
+        dda = DengueDataApi()
+        x_train, x_validate, y_train, y_validate = dda.split_data()
+        sigma2 = 1.
+        poisson_glm = PoissonGLM(x_train=x_train, y_train=y_train, sigma2=sigma2)
+        weights = np.ones(len(x_train)).reshape((-1, 1))
+        obj = lambda w: -poisson_glm.log_joint(y_train.values.reshape((-1, 1)), x_train.values, weights, w, sigma2) / len(x_train)
+        obj_grad = lambda w: -poisson_glm.log_joint_grad(y_train.values.reshape((-1, 1)), x_train.values, weights, w, sigma2) / len(x_train)
+
+        w = 0.5 * np.ones(x_train.shape[1])
+
+        self.assertIsInstance(obj(w), np.float64, msg="log joint not float")
+        self.assertEqual(obj_grad(w).shape, w.shape, msg="log joint grad not float")
+
+    def test_model(self):
         expected_log_joint = 35007.52445104625
         expected_mae = 27.09514170040486
         dda = DengueDataApi()
@@ -16,9 +28,8 @@ class TestModels(TestCase):
         sigma2 = 1.
         poisson_glm = PoissonGLM(x_train=x_train, y_train=y_train, sigma2=sigma2)
 
-        _, w_hist = poisson_glm.compute_posterior_mode()
+        poisson_glm.compute_posterior_mode()
         poisson_glm.compute_laplace_approximation()
-        cov_map = poisson_glm.get_cov_map()
 
         log_joint, mae = poisson_glm.validate_model(x_validate=x_validate, y_validate=y_validate,
                                                     show_posterior_predictive_dist=False)
