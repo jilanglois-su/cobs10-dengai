2827019ef324d42eff678317497a0aabd2d32176
jilanglois <56458805+jilanglois@users.noreply.github.com>
2021-09-14 18:47:53 -0700

    add assert equal to poisson tests

--- tests/test_poisson_glm.py
+++ tests/test_poisson_glm.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 import numpy as np
 from src.d01_data.dengue_data_api import DengueDataApi
 from src.d04_modeling.poisson_glm import PoissonGLM
-
+from expected_values import *
 
 class TestPoissonGlm(TestCase):
 
@@ -12,17 +12,23 @@ class TestPoissonGlm(TestCase):
         sigma2 = 1.
         poisson_glm = PoissonGLM(x_train=x_train, y_train=y_train, sigma2=sigma2)
         weights = np.ones(len(x_train)).reshape((-1, 1))
-        obj = lambda w: -poisson_glm.log_joint(y_train.values.reshape((-1, 1)), x_train.values, weights, w, sigma2) / len(x_train)
-        obj_grad = lambda w: -poisson_glm.log_joint_grad(y_train.values.reshape((-1, 1)), x_train.values, weights, w, sigma2) / len(x_train)
+        obj = lambda w: -poisson_glm.log_joint(y_train.values, x_train.values, weights, w, sigma2) / len(x_train)
+        obj_grad = lambda w: -poisson_glm.log_joint_grad(y_train.values, x_train.values, weights, w, sigma2) / len(x_train)
+        obj_hess = lambda w: -poisson_glm.log_joint_hess(y_train.values, x_train.values, weights, w, sigma2) / len(x_train)
 
         w = 0.5 * np.ones(x_train.shape[1])
 
         self.assertIsInstance(obj(w), np.float64, msg="log joint not float")
-        self.assertEqual(obj_grad(w).shape, w.shape, msg="log joint grad not float")
+        self.assertAlmostEqual(first=expected_obj_log_joint, second=obj(w), places=6,
+                               msg="Error Poisson GLM Log-joint")
+        self.assertEqual(obj_grad(w).shape, w.shape, msg="log joint grad not right size")
+        self.assertMultiLineEqual(first=np.array2string(expected_obj_log_joint_grad, precision=5), second=np.array2string(obj_grad(w), precision=5),
+                                  msg="Error Poisson GLM Log-joint Grad")
+        self.assertEqual(obj_hess(w).shape, (w.shape[0], w.shape[0]), msg="log joint hess not right size")
+        self.assertMultiLineEqual(first=np.array2string(expected_obj_log_joint_hess, precision=5), second=np.array2string(obj_hess(w), precision=5),
+                                  msg="Error Poisson GLM Log-joint Hessian")
 
     def test_model(self):
-        expected_log_joint = 35007.52445104625
-        expected_mae = 27.09514170040486
         dda = DengueDataApi()
         x_train, x_validate, y_train, y_validate = dda.split_data()
         sigma2 = 1.
